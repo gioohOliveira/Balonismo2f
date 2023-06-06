282884e1e7be919d3cc8d2d8e6c04cad65dd83d3
gioohOliveira <go4267994@gmail.com>
2023-06-06 08:28:39 -0300

    aula de orientacao objetos,criar mais objetos:rainha,nome e sobrenome,animal
    Atividade de Fixação do Conteúdo 1 - 2o. Trimestre

--- src/balao.java
+++ src/balao.java
@@ -1,2 +1,15 @@
 public class balao {
+    // Atributos
+    int id;
+    String registro;
+    String cor;
+    String tipo;
+    int capacidade;
+    double altura;
+    String porte;
+    double peso;
+    // Métodos
+    public String voar(){
+        return "voando";
+    }
 }
