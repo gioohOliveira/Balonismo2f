32635b360d0424185c02a93cc0e9fd83d197a206
giooholiveira <go4267994@gmail.com>
2023-08-15 07:40:09 -0300

    Atividade de Fixação do Conteúdo 1 - 2o. Trimestre,atualizei a classe pessoa e piloto

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,3 +1,42 @@
 public class Pessoa {
+    String nome;
+    double CPF;
+    double RG;
+    double Telefone;
+    double Telefonecontato;
+
+    String Riscos;
+
+    public Pessoa(){
+        String nome;
+        private int double cpf;
+        private int double RG;
+        private int double telefone;
+        private int double Telefonecontato;
+    }
+
+    public Pessoa(double CPF,double RG, double Telefone, double Telefonecontato){
+        this.nome="fulano";
+        this.CPF= 1234567890;
+        this.RG= 1236667779;
+        this.Telefone= 551165431;
+        this.Telefonecontato= 551165431;
+    }
+
+    public String setNome(String nome){
+        return nome;
+    }
+
+    public int getRG() {
+        return int RG;
+    }
+
+    public void setRG(double RG){
+     this.RG=1236667779;
+    }
+
+
+    }
+
 
 }
--- src/Piloto.java
+++ src/Piloto.java
@@ -1,2 +1,29 @@
 public class Piloto {
+    String nome;
+    String CPF;
+    double registroregularidade;
+
+
+    public Piloto(){
+    String nome;
+    private int double CPF;
+    private int double registroregularidade;
+    }
+    public Piloto(double CPF, double registroregularidade ) {
+        this.nome = "fulano";
+        private int String CPF = 1234567890;
+        this.registroregularidade = 551165431;
+    }
+    public String setNome(String nome){
+        return nome;
+    }
+
+    public int getregistroregularidade() {
+        return int registroregularidade;
+    }
+    public void setregistroregularidade(double CPF){
+      CPF= 1234567890;
+    }
+
+
 }
